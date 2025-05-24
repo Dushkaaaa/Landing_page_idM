e954a8d77e35676c4fac39d45e58fa27d1f4e7da
Dushkaaaa <dushkevychyevhenii@gmail.com>
2025-05-24 21:40:53 +0200

    add title page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
-"use strict";
+'use strict';
 
-const API = "https://brandstestowy.smallhost.pl/api/random";
+const API = 'https://brandstestowy.smallhost.pl/api/random';
 
 const fetchData = async () => {
   try {
@@ -14,11 +14,11 @@ const fetchData = async () => {
 
     return data;
   } catch (error) {
-    console.error("Fetch error:", error);
+    console.error('Fetch error:', error);
   }
 };
 
-const productBody = document.getElementById("productBody");
+const productBody = document.getElementById('productBody');
 
 fetchData().then((result) => {
   const product = result.data;
@@ -54,11 +54,11 @@ fetchData().then((result) => {
   });
 });
 
-const popup = document.querySelector(".popup");
-const popupDetails = document.querySelector(".details");
+const popup = document.querySelector('.popup');
+const popupDetails = document.querySelector('.details');
 
-productBody.addEventListener("click", function (e) {
-  const card = e.target.closest(".products__card");
+productBody.addEventListener('click', function (e) {
+  const card = e.target.closest('.products__card');
 
   if (!card) {
     return;
@@ -78,21 +78,21 @@ productBody.addEventListener("click", function (e) {
     </div>
     <img src="${image}" alt="${title}" class="details__photo" />
   `;
-  popup.style.display = "block";
+  popup.style.display = 'block';
 });
 
-document.body.addEventListener("click", function (e) {
+document.body.addEventListener('click', function (e) {
   if (
-    e.target.classList.contains("popup__bg") ||
-    e.target.closest(".details__left-side")
+    e.target.classList.contains('popup__bg') ||
+    e.target.closest('.details__left-side')
   ) {
-    popup.style.display = "none";
+    popup.style.display = 'none';
   }
 });
 
-const burgerMenu = document.getElementById("burgerMenu");
-const menu = document.getElementById("menu");
-const closeMenu = document.getElementById("closeMenu");
+const burgerMenu = document.getElementById('burgerMenu');
+const menu = document.getElementById('menu');
+const closeMenu = document.getElementById('closeMenu');
 
-burgerMenu.onclick = () => menu.classList.add("open");
-closeMenu.onclick = () => menu.classList.remove("open");
+burgerMenu.onclick = () => menu.classList.add('open');
+closeMenu.onclick = () => menu.classList.remove('open');
